986d51c295cd50b0adf62b0cad2bca375903f189
nagendra-reddy.bandi <nagendra-reddy.bandi@atos.net>
2023-06-14 05:49:37 +0000

    now the app code is ready to build the app pod

--- src/main/java/com/amos/StartApplication.java
+++ src/main/java/com/amos/StartApplication.java
@@ -12,13 +12,8 @@ public class StartApplication {
 
     @GetMapping("/")
     public String index(final Model model) {
-<<<<<<< HEAD
         model.addAttribute("title", "Tekton + Spring Boot App + Openshift");
-        model.addAttribute("msg", "We are introducing the vault to store the sonarqube token in Tekton on Ohio !!!");
-=======
-        model.addAttribute("title", "Tekton + Spring Boot App + Openshift + PeerTest + imageversion ");
         model.addAttribute("msg", "We are doing PeerTesting with improvments of Tekton on Ohio !!!");
->>>>>>> 4b88b809d6e8808ab42273c87e854a488c257aea
         
         return "index";
     }
